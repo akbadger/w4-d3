f515dc8394461bfccb5b0f0a4082100801d10c95
Aubrey Badger <akbadger@gmail.com>
2017-02-23 10:06:20 -0500

    finished homework, added boxes grow function

--- d3-extra/drawing.js
+++ d3-extra/drawing.js
@@ -18,14 +18,29 @@ function enableDrawing() {
         var box = document.createElement('div');
 
         // these are css values and not javascript - that's why they are strings
+        box.className = 'box';
         box.style.position = 'absolute';
         box.style.top = e.clientY+ 'px';
         box.style.left = e.clientX+ 'px';
         box.style.width = '10px';
-        box.style.height = red + 'px';
+        box.style.height = '20px';
         box.style.borderRadius = '50%';
         box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
         document.body.appendChild(box);
     });
+
+    document.addEventListener('click', function(e) {
+    if (e.target.className === 'box') {
+        var box = e.target;
+        // Number converts the width string style into a number. Replace replaces the px with nothing, and we add px back on to make it a css value once again
+        box.style.width = (Number(box.style.width.replace('px', '')) + 150) + 'px';
+        box.style.height = (Number(box.style.height.replace('px', '')) + 150) + 'px';
+    }
+});
+
 }
+
+
+
+
